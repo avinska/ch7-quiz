b92e2039cfe7684ad869c8c448cdac705116da9e
pamuji0 <bp.pamuji@gmail.com>
2022-04-10 12:08:26 +0700

    add controller and views create

--- controller/index.js
+++ controller/index.js
@@ -8,7 +8,20 @@ const Home = async (req, res) => {
     })
 }
 
+//route create
+const getAdd =(_,res)=>{
+    res.render('../views/addProduct.ejs')
+}
+const postAdd=async(req,res)=>{
+    const product=await Product.create({
+        product_name:req.body.product_name,
+        description : req.body.description,
+        price:req.body.price,
+        img_url: req.body.img_url,
+    });
+    res.redirect('/')
+}
 
 module.exports = {
-    Home
+    Home,getAdd,postAdd
 }
