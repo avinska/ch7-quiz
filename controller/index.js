dec325b6872251ecb04a5cf09ae9853bfc51cc61
Soraya Isfandiari <soraya.isf@gmail.com>
2022-04-11 05:39:21 +0700

    add new READ file

--- controller/index.js
+++ controller/index.js
@@ -1,32 +1,39 @@
-const { Product, Stock } = require('../models')
+const {
+    Product,
+    Stock
+} = require('../models')
 
 //route to home page
 const Home = async (req, res) => {
-    const productList = await Product.findAll()
+    const productList = await Product.findAll({
+        include: ['stock']
+    })
     res.render('productDetails', {
         data: productList
     })
 }
 
 //route create
-const getAdd =(_,res)=>{
+const getAdd = (_, res) => {
     res.render('addProduct')
 }
-const postAdd=async(req,res)=>{
-    const product= await Product.create({
-        product_name:req.body.product_name,
-        description : req.body.description,
-        price:req.body.price,
+const postAdd = async (req, res) => {
+    const product = await Product.create({
+        product_name: req.body.product_name,
+        description: req.body.description,
+        price: req.body.price,
         img_url: req.body.img_url,
     });
     await Stock.create({
-        sold:req.body.sold,
-        in_stock:req.body.in_stock,
-        product_id:product.uuid
+        sold: req.body.sold,
+        in_stock: req.body.in_stock,
+        product_id: product.uuid
     })
     res.redirect('/')
 }
 
 module.exports = {
-    Home,getAdd,postAdd
+    Home,
+    getAdd,
+    postAdd
 }
